790d3d0b4adfc6aa8d9b0f68ae27c84606016b28
Richard Braun <rbraun@sceen.net>
2017-10-31 12:09:11 +0100

    Add some initial documentation

--- qemu.sh
+++ qemu.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Safely create a temporary directory.
 TMPDIR=$(mktemp -d)
 CDROOT=$TMPDIR/cdroot
 
+# Build a cdrom image with both the GRUB boot loader and the kernel binary.
 mkdir -p $CDROOT/boot/grub
 cp x1 $CDROOT/boot
 cat > $CDROOT/boot/grub/grub.cfg << EOF
@@ -14,12 +16,23 @@ menuentry "X1" --class os {
 EOF
 grub-mkrescue -o $TMPDIR/grub.iso $CDROOT
 
+# Start the QEMU emulator with options doing the following :
+#  - GDB remote access on the local TCP port 1234
+#  - 64MiB of physical memory (RAM)
+#  - No video device (automatically sets the first COM port as the console)
+#  - Boot from the generated cdrom image.
+#
+# In order to dump all exceptions and interrupts to a log file, you may add
+# the following options :
+#       -d int \
+#       -D int_debug.log \
+#
+# Note that these debugging options do not work when KVM is enabled.
 qemu-system-i386 \
-          -enable-kvm \
-          -gdb tcp::1234 \
-          -m 64 \
-          -nographic \
-          -cdrom $TMPDIR/grub.iso \
-          -boot d
+        -gdb tcp::1234 \
+        -m 64 \
+        -nographic \
+        -cdrom $TMPDIR/grub.iso \
+        -boot d
 
 rm -rf $TMPDIR
