ac37c1429786d61ad5058a264e2b53a700f017aa
Richard Braun <rbraun@sceen.net>
2017-11-24 15:08:31 +0100

    assert: improve nop version

--- assert.h
+++ assert.h
@@ -25,7 +25,14 @@
 #define _ASSERT_H
 
 #ifdef NDEBUG
-#define assert(expression)
+
+/*
+ * The assert() macro normally doesn't produce side effects when turned off,
+ * but this may result in many "set but not used" warnings. Using sizeof()
+ * silences these warnings without producing side effects.
+ */
+#define assert(expression) ((void)sizeof(expression))
+
 #else /* NDEBUG */
 
 #include <macros.h>
